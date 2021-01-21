0ea6c2812328949687fc3c0733d07fbde9992d7a
Antonio <thetonybl@gmail.com>
2021-01-20 21:34:16 -0600

    ESLint

---
+++ src/index.js
@@ -0,0 +1,5 @@
+import React from 'react';
+import ReactDOM, { unstable_renderSubtreeIntoContainer } from 'react-dom';
+import HelloWold from './components/HelloWold'
+
+ReactDOM.render( < HelloWold / > , document.getElementById('app'));
